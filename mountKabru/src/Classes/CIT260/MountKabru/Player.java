2c6bf2035d8d3ad9612e2be4ef017db5f840b31a
David Young <davey.1@live.com>
2016-01-28 10:03:38 -0700

    1-27-16 start project

---
+++ mountKabru/src/Classes/CIT260/MountKabru/Player.java
@@ -0,0 +1,193 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes.CIT260.MountKabru;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author David
+ */
+public class Player implements Serializable{
+    private String name;
+    private String attribute;
+    private String inventory;
+    private double strength;
+    private double dexterity;
+    private double mana;
+    private double health;
+    private String spell;
+    private double defense;
+    private double level;
+    private String attack;
+
+    public Player() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(String inventory) {
+        this.inventory = inventory;
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
+    }
+
+    public double getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(double dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public double getMana() {
+        return mana;
+    }
+
+    public void setMana(double mana) {
+        this.mana = mana;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public String getSpell() {
+        return spell;
+    }
+
+    public void setSpell(String spell) {
+        this.spell = spell;
+    }
+
+    public double getDefense() {
+        return defense;
+    }
+
+    public void setDefense(double defense) {
+        this.defense = defense;
+    }
+
+    public double getLevel() {
+        return level;
+    }
+
+    public void setLevel(double level) {
+        this.level = level;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+
+    public void setAttack(String attack) {
+        this.attack = attack;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.attribute);
+        hash = 83 * hash + Objects.hashCode(this.inventory);
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.strength) ^ (Double.doubleToLongBits(this.strength) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.dexterity) ^ (Double.doubleToLongBits(this.dexterity) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.mana) ^ (Double.doubleToLongBits(this.mana) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.health) ^ (Double.doubleToLongBits(this.health) >>> 32));
+        hash = 83 * hash + Objects.hashCode(this.spell);
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.defense) ^ (Double.doubleToLongBits(this.defense) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.level) ^ (Double.doubleToLongBits(this.level) >>> 32));
+        hash = 83 * hash + Objects.hashCode(this.attack);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", attribute=" + attribute + ", inventory=" + inventory + ", strength=" + strength + ", dexterity=" + dexterity + ", mana=" + mana + ", health=" + health + ", spell=" + spell + ", defense=" + defense + ", level=" + level + ", attack=" + attack + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.strength) != Double.doubleToLongBits(other.strength)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.dexterity) != Double.doubleToLongBits(other.dexterity)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.mana) != Double.doubleToLongBits(other.mana)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.health) != Double.doubleToLongBits(other.health)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.defense) != Double.doubleToLongBits(other.defense)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.level) != Double.doubleToLongBits(other.level)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.attribute, other.attribute)) {
+            return false;
+        }
+        if (!Objects.equals(this.inventory, other.inventory)) {
+            return false;
+        }
+        if (!Objects.equals(this.spell, other.spell)) {
+            return false;
+        }
+        if (!Objects.equals(this.attack, other.attack)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
+
--- mountKabru/src/mountkabru/MountKabru.java
+++ mountKabru/src/mountkabru/MountKabru.java
@@ -5,6 +5,8 @@
  */
 package mountkabru;
 
+import Classes.CIT260.MountKabru.Player;
+
 /**
  *
  * @author David
@@ -15,7 +17,18 @@ public class MountKabru {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+        
+        playerOne.setAttack("happy");
+        playerOne.setDefense(7);
+        playerOne.setDexterity(2);
+        playerOne.setHealth(9000);
+        playerOne.setInventory("inventory");
+        playerOne.setLevel(-3);
+        playerOne.setMana(5);
+        playerOne.setName("Joe");
+        playerOne.setSpell("lots");
+        playerOne.setStrength(44);
     }
     
 }
