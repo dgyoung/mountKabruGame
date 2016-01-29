b3b2ab4a2c46792be793fe84c87ef89308754eec
David Young <davey.1@live.com>
2016-01-29 09:45:05 -0700

    1-27-16 start project

---
+++ mountKabru/src/Classes/CIT260/MountKabru/Location.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes.CIT260.MountKabru;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author David
+ */
+public class Location implements Serializable{
+    private int level;
+    private int stage;
+
+    public Location() {
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.level) ^ (Double.doubleToLongBits(this.level) >>> 32));
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.stage) ^ (Double.doubleToLongBits(this.stage) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "level=" + level + ", stage=" + stage + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Double.doubleToLongBits(this.level) != Double.doubleToLongBits(other.level)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.stage) != Double.doubleToLongBits(other.stage)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
---
+++ mountKabru/src/Classes/CIT260/MountKabru/Question.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes.CIT260.MountKabru;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author David
+ */
+public class Question implements Serializable{
+    private String actorName;
+    private String answer;
+
+    public Question() {
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + Objects.hashCode(this.actorName);
+        hash = 31 * hash + Objects.hashCode(this.answer);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (!Objects.equals(this.actorName, other.actorName)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer, other.answer)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
