7f3b0f2b5c2d312c9862cb5ecc2403bfafd31146
JaredHerbert <jared.herbert6@gmail.com>
2016-02-11 09:39:44 -0700

    added spell attack 2/11/16

--- mountKabru/src/byui/MountKabru/Control/ActorsControl.java
+++ mountKabru/src/byui/MountKabru/Control/ActorsControl.java
@@ -15,7 +15,7 @@ public class ActorsControl {
     if (attack < 5 || attack > 100){
         return -1;	
                 }
-    if (mana < 5 || mana > 103 ){
+    if (mana < 5 || mana > 305 ){
         return -2;
     }
     if (defence < 5 || defence > 100){
