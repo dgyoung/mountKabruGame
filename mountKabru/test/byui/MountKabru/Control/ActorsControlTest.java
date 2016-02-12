bd55287c730b711f80e522613e670796604e4b8a
JaredHerbert <jared.herbert6@gmail.com>
2016-02-12 09:33:33 -0700

    added player and actor test attack 2/12/16

---
+++ mountKabru/test/byui/MountKabru/Control/ActorsControlTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.MountKabru.Control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Moose
+ */
+public class ActorsControlTest {
+    
+    public ActorsControlTest() {
+    }
+
+    /**
+     * Test of spellAttack method, of class ActorsControl.
+     */
+    @Test
+    public void testSpellAttack() {
+        System.out.println("spellAttack");
+        double mana = 20.0;
+        double attack = 25.0;
+        double defence = 15.0;
+        ActorsControl instance = new ActorsControl();
+        double result = instance.spellAttack(mana, attack, defence);
+        assertTrue(result > 30 && result < 41);
+        // TODO review the generated test code and remove the default call to fail.
+        }
+
+    /**
+     * Test of strangthAttack method, of class ActorsControl.
+     */
+    @Test
+    public void testStrangthAttack() {
+        System.out.println("strangthAttack");
+        double strangth = 20.0;
+        double attack = 25.0;
+        double defence = 15.0;
+        ActorsControl instance = new ActorsControl();
+        double result = instance.strangthAttack(strangth, attack, defence);
+        assertTrue(result >30 && result < 41);
+        // TODO review the generated test code and remove the default call to fail.
+        }
+    
+}
