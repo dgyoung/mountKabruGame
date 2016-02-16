d2233cdad0e821306163d5f4437ea714d5468475
David Young <davey.1@live.com>
2016-02-16 09:58:42 -0700

    Added levelUp function in PlayerControl 2-13-16

--- mountKabru/src/byui/MountKabru/Control/PlayerControl.java
+++ mountKabru/src/byui/MountKabru/Control/PlayerControl.java
@@ -44,67 +44,73 @@ public class PlayerControl {
     }
     
     
-    public void levelUp (double level, double exp, double attack, double dextarity, double defence, double mana, String attribute) {
-        
+    public void levelUp (Player player, double exp, String attribute) {
+        double level = player.getLevel();
+        double attack = player.getAttack();
+        double dexterity = player.getDexterity();
+        double defense = player.getDefense();
+        double mana = player.getMana();
         if ((level * 2) < attack){
             System.out.println(" currupt save " );
         } 
-        if ((level * 2) < dextarity){
+        if ((level * 2) < dexterity){
             System.out.println(" currupt save " );
         } 
-        if ((level * 2) < defence){
+        if ((level * 2) < defense){
             System.out.println(" currupt save " );
         } 
         if ((level * 2) < mana){
             System.out.println(" currupt save " );
         } 
-        Player saveA = new Player();
+        
         if (exp >= 10 * level * level){
-            int random = (int )(Math.random() * 3);
-            if ("bard".equals(attribute)){
-                random = (int )(Math.random() * 3);
-                attack = attack + random + 3;
-                random = (int )(Math.random() * 3);
-                dextarity = dextarity + random + 3;
-                random = (int )(Math.random() * 3);
-                defence = defence + random + 1;
-                random = (int )(Math.random() * 3);
-                mana = mana + random + 3;
-                saveA.setAttack(attack);
-                saveA.setDexterity(dextarity);
-                saveA.setDefense(defence);
-                saveA.setMana(mana);
-            }
-            else if ("warrior".equals(attribute)){
-                random = (int )(Math.random() * 3);
-                attack = attack + random + 4;
-                random = (int )(Math.random() * 3);
-                dextarity = dextarity + random + 2;
-                random = (int )(Math.random() * 3);
-                defence = defence + random + 2;
-                random = (int )(Math.random() * 3);
-                mana = mana + random + 2;
-                saveA.setAttack(attack);
-                saveA.setDexterity(dextarity);
-                saveA.setDefense(defence);
-                saveA.setMana(mana);
-            }
-            else if ("mage".equals(attribute)){
-                random = (int )(Math.random() * 3);
-                attack = attack + random + 2;
-                random = (int )(Math.random() * 3);
-                dextarity = dextarity + random + 2;
-                random = (int )(Math.random() * 3);
-                defence = defence + random + 1;
-                random = (int )(Math.random() * 3);
-                mana = mana + random + 4;
-                saveA.setAttack(attack);
-                saveA.setDexterity(dextarity);
-                saveA.setDefense(defence);
-                saveA.setMana(mana);
-            }
-            else {
-                System.out.println(" invalid class " );
+            int random;
+            switch (attribute) {
+                case "bard":
+                    random = (int )(Math.random() * 3);
+                    attack = attack + random + 3;
+                    random = (int )(Math.random() * 3);
+                    dexterity = dexterity + random + 3;
+                    random = (int )(Math.random() * 3);
+                    defense = defense + random + 1;
+                    random = (int )(Math.random() * 3);
+                    mana = mana + random + 3;
+                    player.setAttack(attack);
+                    player.setDexterity(dexterity);
+                    player.setDefense(defense);
+                    player.setMana(mana);
+                    break;
+                case "warrior":
+                    random = (int )(Math.random() * 3);
+                    attack = attack + random + 4;
+                    random = (int )(Math.random() * 3);
+                    dexterity = dexterity + random + 2;
+                    random = (int )(Math.random() * 3);
+                    defense = defense + random + 2;
+                    random = (int )(Math.random() * 3);
+                    mana = mana + random + 2;
+                    player.setAttack(attack);
+                    player.setDexterity(dexterity);
+                    player.setDefense(defense);
+                    player.setMana(mana);
+                    break;
+                case "mage":
+                    random = (int )(Math.random() * 3);
+                    attack = attack + random + 2;
+                    random = (int )(Math.random() * 3);
+                    dexterity = dexterity + random + 2;
+                    random = (int )(Math.random() * 3);
+                    defense = defense + random + 1;
+                    random = (int )(Math.random() * 3);
+                    mana = mana + random + 4;
+                    player.setAttack(attack);
+                    player.setDexterity(dexterity);
+                    player.setDefense(defense);
+                    player.setMana(mana);
+                    break;
+                default:
+                    System.out.println(" invalid class " );
+                    break;
             }
         }
     }
