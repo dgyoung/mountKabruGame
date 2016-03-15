7d54c1ba33e0b29603229461d99683b2745939fa
JaredHerbert <jared.herbert6@gmail.com>
2016-03-15 11:13:48 -0700

    main menu 3 / 15 / 16  map work  10:00    jared herbert

--- mountKabru/src/byui/MountKabru/Control/MapControl.java
+++ mountKabru/src/byui/MountKabru/Control/MapControl.java
@@ -13,9 +13,13 @@ import Classes.CIT260.MountKabru.Scene;
  * @author Moose
  */
 public class MapControl {
+    static ArrayList <Location>createLocation(){
+         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    } 
+    
     public static Map createMap() {
     // create the map
-Map map = new Map (20, 20);
+Map map = new Map (5, 10);
 
 // create a list of the different scenes in the game
 Scene[] scenes = createScenes();
@@ -23,10 +27,18 @@ Scene[] scenes = createScenes();
 // assigne the different scenes to locations in the map
 assignScenesToLocations(map, scenes);
 
+
 return map;
+    }
+    static void moveActorsToStartingLocation(Map map) {
+        
+    }
+    private static void assignScenesToLocations(Map map,Scene[] scenes){
+        
     }
 
     private static Scene[] createScenes() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
     }
