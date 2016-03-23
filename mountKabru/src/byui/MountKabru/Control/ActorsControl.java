f1848266f21900dfe3d4b2501e131052af3ffe74
David Young <davey.1@live.com>
2016-03-23 10:00:15 -0600

    3/23 added error control

--- mountKabru/src/byui/MountKabru/Control/ActorsControl.java
+++ mountKabru/src/byui/MountKabru/Control/ActorsControl.java
@@ -5,24 +5,33 @@
  */
 package byui.MountKabru.Control;
 
+import Classes.CIT260.MountKapru.exceptions.AttackControlException;
+
 /**
  *
  * @author Moose
  */
 public class ActorsControl {
-    public double spellAttack (double mana, double attack, double defence) {
-
+    public void spellAttack (double mana, double attack, double defence) throws AttackControlException {
+        
     if (attack < 5 || attack > 305){
-        return -1;	
-                }
+        throw new AttackControlException("invalid save file "
+                + "please start a new game");	
+    }
     if (mana < 5 || mana > 305 ){
-        return -2;
+        throw new AttackControlException("invalid save file "
+                + "please start a new game");	
+                
     }
     if (defence < 5 || defence > 305){
-        return -3;
+        throw new AttackControlException("invalid save file "
+                + "please start a new game");	
+                
     }
     double spellDamage = ((attack + mana)- defence) + (Math.random() * 10);
-    return spellDamage;
+    if (spellDamage > 0){
+        return spellDamage;
+    }
     }
 
 
