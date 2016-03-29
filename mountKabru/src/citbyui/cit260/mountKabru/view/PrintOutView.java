53cc9d8a517a21f4c75a51085f6f0ea430a8f223
JaredHerbert <jared.herbert6@gmail.com>
2016-03-29 15:39:43 -0600

    save file work jared herbert lesson 11 personal assingnment 2

--- mountKabru/src/citbyui/cit260/mountKabru/view/PrintOutView.java
+++ mountKabru/src/citbyui/cit260/mountKabru/view/PrintOutView.java
@@ -5,7 +5,7 @@
  */
 package citbyui.cit260.mountKabru.view;
 
-import Classes.CIT260.MountKabru.Actors;
+import Classes.CIT260.MountKabru.Actor;
 import byui.MountKabru.Control.GameControl;
 import byui.MountKabru.exceptions.GameControlException;
 import java.io.IOException;
@@ -23,7 +23,7 @@ public class PrintOutView extends View {
                 + "\n | Dapper Dan pulls out a quill and asks                        |"
                 + "\n | 'so what do you want me to write ?'                            |"
                 + "\n------------------------------------------------------------------------------"
-                + "\nN - Write out a list for Monsters"
+                + "\nN - Write out a list for Monsters and there location"
                 + "\nQ - Quit"
                 + "\n-------------------------------------------------------------------------------");
 
@@ -39,7 +39,7 @@ public class PrintOutView extends View {
         
         switch (choice) {
             case "N":
-                this.PrintMonsters();
+                this.monsterList();
                 break;
             default:
                 ErrorView.display("PrintOutView ", "\n*** Invalid selection *** Try again");
@@ -48,60 +48,27 @@ public class PrintOutView extends View {
         return false;
     }
 
-    private void PrintMonsters() {
-        String value = " ";
-   
-      this.console.println(" Where would you like to save this list? ");
-      
-         try {
-                value = this.keyboard.readLine();
-                value = value.trim();
-    }
-    catch (Exception e ) {
-               ErrorView.display("StartProgramView", "Key board Error: " + e.getMessage());
-    }
- 
-     String filePath = value;
-        Actors[] actors = MountKabru.getCurrentGame().getActors();
-        String listOMonsters = null;
-        try {
-        listOMonsters = GameControl.listOfTheMonsters(actors);
-        } catch (GameControlException e) {
-            e.printStackTrace();
-
-   
-
-            }
-    }
-String monsterList = "\n"
+   public void monsterList( ) {
+    Actor[] monsters = Actor.values();
+    String monsterList = "\n"
                 + "\n|--------------------------------------------------|"
                 + "\n|                                                  |"
                 + "\n|     ****        Monster  List     ****       |"
                 + "\n|                                                  |"
                 + "\n|--------------------------------------------------|"
                 + "\n The Monsters                               "
-                + "\n----------------------------------------------------"
+                + "\n----Monters-------------------Locations-----------------------------"
           
                 + "\n--------------------------------------";
+    for (Actor actors : monsters) {
+        System.out.println(actors + ":    \t" + actors.getName() + actors.getLocation());
+    }
+}
 
-        FileOutputStream fops = null;
-        try {
-            fops = new FileOutputStream(filePath);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ObjectOutputStream output = null;
-        try {
-            output = new ObjectOutputStream(fops);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+   
 
-        try {
-            output.writeObject(monsterReport); //write game to object File
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        
-        this.console.println(monsterReport);
+            }
+
+
+
+  
